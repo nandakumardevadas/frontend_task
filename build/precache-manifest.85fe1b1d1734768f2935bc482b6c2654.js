be8a557434872be5851c0b35be314e1846b485a3
TechSquad404 <TechSquad404@gmail.com>
2020-05-21 20:00:42 +0530

    Prod build updated

---
+++ build/precache-manifest.85fe1b1d1734768f2935bc482b6c2654.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0742e29be17ebf68a16435e1ccc7d96b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c28a84fc4f9d4099a46",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "e495b4903c703316f4c0",
+    "url": "/static/css/main.b6dd56b7.chunk.css"
+  },
+  {
+    "revision": "0c28a84fc4f9d4099a46",
+    "url": "/static/js/2.c5930caf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c5930caf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e495b4903c703316f4c0",
+    "url": "/static/js/main.ef04fccc.chunk.js"
+  },
+  {
+    "revision": "6e204fa7a0a0af4a4739",
+    "url": "/static/js/runtime-main.5488b41c.js"
+  },
+  {
+    "revision": "d6bee12c7e838e84074faeb7c131c988",
+    "url": "/static/media/MYRIADPRO-BOLD.d6bee12c.woff"
+  },
+  {
+    "revision": "2e7c21c38bb6bc58696a5b78c8daca83",
+    "url": "/static/media/MYRIADPRO-BOLDCOND.2e7c21c3.woff"
+  },
+  {
+    "revision": "77667b199ab79554bc83124b915bcb18",
+    "url": "/static/media/MYRIADPRO-BOLDCONDIT.77667b19.woff"
+  },
+  {
+    "revision": "c9b11d38b2b59d9b9af854e5e6a393bc",
+    "url": "/static/media/MYRIADPRO-BOLDIT.c9b11d38.woff"
+  },
+  {
+    "revision": "8b8a29e2f40541f5622a488a4fbcf316",
+    "url": "/static/media/MYRIADPRO-COND.8b8a29e2.woff"
+  },
+  {
+    "revision": "f6185ef4169956b114309589af2daac8",
+    "url": "/static/media/MYRIADPRO-CONDIT.f6185ef4.woff"
+  },
+  {
+    "revision": "39338481a9b2c3753b61115062caf9b4",
+    "url": "/static/media/MYRIADPRO-REGULAR.39338481.woff"
+  },
+  {
+    "revision": "6fd5c55d90e96c8af1041794ab70f5f2",
+    "url": "/static/media/MYRIADPRO-SEMIBOLD.6fd5c55d.woff"
+  },
+  {
+    "revision": "5487750ab49a9097bdf00c02ddc58c93",
+    "url": "/static/media/MYRIADPRO-SEMIBOLDIT.5487750a.woff"
+  },
+  {
+    "revision": "d1846886b009a596fef09f1b69f9b548",
+    "url": "/static/media/MyriadPro-Light.d1846886.woff"
+  },
+  {
+    "revision": "a8c50712eb8e86a3f56738594a9b62aa",
+    "url": "/static/media/bg.a8c50712.png"
+  },
+  {
+    "revision": "66a4e1a8f05c481df65c1edcb4b32c5b",
+    "url": "/static/media/bg_work.66a4e1a8.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "58eb2a15f7d73059bb87b9de22282633",
+    "url": "/static/media/ic_folder_setting.58eb2a15.svg"
+  },
+  {
+    "revision": "23111b1eb4cd5e65e96bd838b77c31d2",
+    "url": "/static/media/ic_staffing.23111b1e.svg"
+  },
+  {
+    "revision": "4e883040a3ee83ef8d18417ac9b899c9",
+    "url": "/static/media/ic_technical.4e883040.svg"
+  },
+  {
+    "revision": "ce42593091b64eb830605df429c7c373",
+    "url": "/static/media/map.ce425930.png"
+  }
+]);
